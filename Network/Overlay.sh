7cfae2147b3fb8423ff456042783760f81f871f1
liedserver <121692612+liedserver@users.noreply.github.com>
2023-01-03 14:18:45 -0300

    Create Overlay.sh

---
+++ Network/Overlay.sh
@@ -0,0 +1,12 @@
+    #cria uma nova rede overlay
+    docker network create
+    #lista as redes overlay disponíveis.
+    docker network ls: 
+    #exibe informações detalhadas sobre uma rede overlay.
+    docker network inspect
+    #conecta um contêiner a uma rede overlay.
+    docker network connect 
+    #desconecta um contêiner de uma rede overlay.
+    docker network disconnect 
+    #remove uma rede overlay.
+    docker network rm 
