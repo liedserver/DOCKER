a04600b2849055823e1dfed977c9b84a5583ee7c
liedserver <121692612+liedserver@users.noreply.github.com>
2023-01-03 14:34:33 -0300

    Create Memory.sh

---
+++ Limits/Memory.sh
@@ -0,0 +1,11 @@
+    #define o limite de memória em bytes que um contêiner pode usar.
+    docker run --memory 
+    #define o limite total de memória e troca (swap) que um contêiner pode usar.
+    docker run --memory-swap 
+    
+    ##############
+    docker stats
+    
+    ##############
+    docker update
+    
