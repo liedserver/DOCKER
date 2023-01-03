099cdca1dd9bd070697b3aa098a3701ba05b26e7
liedserver <121692612+liedserver@users.noreply.github.com>
2023-01-03 14:33:19 -0300

    Create Cpu.sh

---
+++ Limits/Cpu.sh
@@ -0,0 +1,11 @@
+    #define o número de "shares" de CPU que um contêiner pode usar. Quanto maior o número de shares, mais CPU o contêiner pode usar.
+    docker run --cpu-shares 
+    #define o limite de processos que um contêiner pode criar.
+    docker run --pids-limit 
+    
+    #############
+    docker stats
+    
+    ############
+    docker update
+    
